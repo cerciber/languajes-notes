af0448bc311ae72ba324369901778b754b933a1a
Cesar Torres <ceatorresar@unal.edu.co>
2020-01-07 10:33:50 -0500

    Python RegEx secuencias especiales

---
+++ 21. Phyton/44. Expresiones regulares (Re)/03. Conjuntos.py
@@ -0,0 +1,5 @@
+
+# Conjuntos (se separan por []) (Los caracteres especiales no aplican aqui)
+expresion_regular = "[caracter1caracter2caracterN]"		# Cualquiera de los caracteres especificados (OR)
+expresion_regular = "[caracter1-caracter2]"				# Cualquier caracter entre el rango especificado
+expresion_regular = "[^caracter1caracter2caracterN]"	# Cualquier caracter excepto los caracteres especificados
